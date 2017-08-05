42c9a11f4c84952b7ddb6bc67c85b72d872a964f
Remi Tobias <remitobias@Remis-MacBook-Pro.local>
2017-08-05 16:57:56 +0100

    Added web view

--- WLCApp/WLCApp/ViewController.swift
+++ WLCApp/WLCApp/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let authorizationURL = URL(string:"http://westlondoncoders.com/")
+        let request = URLRequest(url: authorizationURL!)
+        webView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
