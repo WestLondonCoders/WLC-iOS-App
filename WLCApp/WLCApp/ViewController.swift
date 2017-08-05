46581389352a59872f8da41f0efadb0582ae91ec
Adekunle77 <g.77@virgin.net>
2017-08-05 16:33:36 +0100

    Created Xcode Project

---
+++ WLCApp/WLCApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WLCApp
+//
+//  Created by Ade Adegoke on 05/08/2017.
+//  Copyright © 2017 WLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
