6db135e12578ecab454acd8353c005ea9bfb8722
Remi Tobias <remitobias@Remis-MacBook-Pro.local>
2017-09-16 15:32:51 +0100

    Created a custom TableView Cell

---
+++ WLCApp/WLCApp/LanguageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LanguageTableViewCell.swift
+//  WLCApp
+//
+//  Created by Remi Tobias on 16/09/2017.
+//  Copyright © 2017 WLC. All rights reserved.
+//
+
+import UIKit
+
+class LanguageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
